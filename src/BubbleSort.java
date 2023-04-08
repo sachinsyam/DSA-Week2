7acf13310757fc34d80d3485d3a548757dd02af7
Sachin <sachinsyam@gmail.com>
2023-04-08 11:48:56 +0530

    first commit. bubble sort with recursion.

---
+++ src/BubbleSort.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int[] arr = new int[10];
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = r.nextInt(1000);
+        }
+        boolean shifted = true;
+
+        print(arr);
+
+        while(shifted){
+            shifted = false;
+            for (int i = 0; i < arr.length-1; i++) {
+                if(arr[i] > arr[i+1]){
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                    shifted = true;
+                }
+            }
+        }
+        System.out.println("\nSorted");
+        print(arr);
+
+        int [] sortedArray = recursion(arr,true);
+        print(sortedArray);
+
+    }
+
+    static void print(int [] arr){
+        System.out.println();
+        Arrays.stream(arr).forEach(a -> System.out.print(a+" "));
+    }
+
+    static int[] recursion(int [] arr, boolean shifted){
+        if(shifted){
+            for (int i = 0; i <arr.length-1 ; i++) {
+                shifted = false;
+                if(arr[i] > arr[i+1]){
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                    shifted = true;
+                }
+            }
+            return recursion(arr,shifted);
+        }
+        return arr;
+    }
+
+}
